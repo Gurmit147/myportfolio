28c903789f3d8f53b7f54e9f1776cb5dbf00e693
Gurmit147 <129723655+Gurmit147@users.noreply.github.com>
2024-12-24 20:45:43 +0800

    Initial commit

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Sidebar from '../components/sidebar';
+import '../css/webstructure.css';
+
 function Homepage() {
-    return(
+    return (
         <div>
-            <Sidebar/>
+            <Sidebar />
+            <div className='intro-content flex flex-col md:flex-row items-center justify-center h-screen w-[90%] p-8 md:ml-0 ml-32 '>
+                <div className='text-center md:text-left md:w-1/2'>
+                    <h1 className='text-5xl md:text-6xl font-bold mb-6'>Hi,</h1>
+                    <h1 className='text-6xl md:text-7xl font-semibold mb-6'>I'm Gurmit</h1>
+                    <h1 className='text-4xl md:text-5xl text-gray-600'>Software Engineer</h1>
+                </div>
+                <div className='md:w-1/2 mt-8 md:mt-0'>
+                    <img src='images/developer.png' alt='Developer' className='max-w-full h-auto rounded-lg shadow-lg' />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
 export default Homepage;
